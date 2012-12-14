6461697d2efd1c9ee230367830537279a0fd735f
rd07991 <none@none>
2012-12-14 17:35:31 +0100

    RD - Code went satisfactory through validation (GoM) after several changes. 	- Implemented LS fit of Rankine vortex 	- Checked all parameters in eddy_core_diameter (getScales) 	- Able to save and reload data. TODO : implement lanczos filter before filtering velocities

--- kernel/bins.py
+++ kernel/bins.py
@@ -1,6 +1,7 @@
 import numpy as np
 from altimetry.externals.esutils_stat import histogram
 import altimetry.tools as AT
+if __debug__ : import matplotlib.pyplot as plt
 
 def grid_time(time,tid,binsize=1):
     '''
@@ -19,9 +20,19 @@ def grid_time(time,tid,binsize=1):
     hist,R = histogram(tid,binsize=binsize, min=0, max=nt-1, rev=True, use_weave=False, verbose=0)
     ind = AT.histogram_indices(hist, R)
     
-    btime=time.copy()
-    btime[:]=0.
-    for i in np.arange(nt) : btime[i]=np.mean(time[tid[ind[i]]])
+    hist=np.ma.masked_array(hist,mask=hist==0,dtype=np.float32)
+    hist.data[hist.mask]=hist.fill_value
+    
+    btime = np.ma.masked_array(np.zeros(len(hist)),mask=hist.mask,dtype=time.dtype)
+#    btime=time.copy()
+#    btime.data[:]=btime.fill_value
+    for i in np.arange(len(hist)) : btime[i]=np.mean(time[tid[ind[i]]])
+#    for i in np.arange(len(hist)) : blon[i]=np.mean(lon[eid[ind[i]]])
+    
+    
+    
+#    #normalise histogram with binsize
+#    hist/=np.float(binsize)
     
     return hist, ind, btime
     
@@ -46,14 +57,22 @@ def grid_space(lon,lat,eid,binsize=7):
     hist,R = histogram(eid,binsize=binsize, rev=True, use_weave=False, verbose=0)
     ind = AT.histogram_indices(hist, R)
     
-    blon = np.repeat(np.NaN,len(hist))
-    blat = np.repeat(np.NaN,len(hist))
-    for i in np.arange(len(hist)) : blon[i]=np.mean(lon[eid[ind[i]]])
-    for i in np.arange(len(hist)) : blat[i]=np.mean(lat[eid[ind[i]]])
+    hist=np.ma.masked_array(hist,mask=hist==0,dtype=np.float32)
+    hist.data[hist.mask]=hist.fill_value
+    
+    blon = np.ma.masked_array(np.zeros(len(hist)),mask=hist.mask,dtype=lon.dtype)
+    blat = np.ma.masked_array(np.zeros(len(hist)),mask=hist.mask,dtype=lon.dtype)
+    for i,j in enumerate(hist.mask) :
+        if ~j : blon[i]=np.mean([np.max(lon[eid[ind[i]]]),np.min(lon[eid[ind[i]]])])
+    for i,j in enumerate(hist.mask) :
+        if ~j: blat[i]=np.mean([np.max(lat[eid[ind[i]]]),np.min(lat[eid[ind[i]]])])
+    
+#    #normalise histogram with binsize
+#    hist/=np.float(binsize)
     
     return hist, ind, blon, blat
 
-def grid_var(var,hist,ind):
+def grid_var(var,hist,ind,method='mean'):
     '''
     GRID_VAR
     @summary: Compute statistics of a given variable in the gridded (space/time) frame.
@@ -66,29 +85,41 @@ def grid_var(var,hist,ind):
     '''
     
     nx=len(hist)
-    mn = np.repeat(np.NaN,nx)
-    rms = np.repeat(np.NaN,nx)
-    for i in np.arange(nx) : mn[i]=np.mean(var[ind[i]])
-    for i in np.arange(nx) : rms[i]=AT.rms(var[ind[i]])
+    mn = np.ma.masked_array(np.zeros(nx),mask=np.ones(nx,dtype=bool))
+    md = np.ma.masked_array(np.zeros(nx),mask=np.ones(nx,dtype=bool))
+    rms = np.ma.masked_array(np.zeros(nx),mask=np.ones(nx,dtype=bool))
+    mn.data[mn.mask]=mn.fill_value
+    md.data[md.mask]=md.fill_value
+    rms.data[rms.mask]=rms.fill_value
+    for i,j in enumerate(np.array(ind)) :
+        if ~hist.mask[i] :
+            mn[i]=np.mean(var[j])
+            md[i]=np.median(var[j])
+#            if method == 'mean' : mn[i]=np.mean(var[j])
+#            elif method == 'median' : mn[i]=np.median(var[j])
+#            else : raise Exception('Unknown averaging method')
+            rms[i]=AT.rms(var[j])
     
-    return mn, rms
+    if method == 'mean' : return mn, rms 
+    elif method == 'median' : return md, rms
+    else : raise Exception('Unknown averaging method')
 
-def bin_space(lon,lat,eind,amplitude,diameter,relvort,ugdiameter,wvdiameter):
-    hist, ind, blon, blat = grid_space(lon,lat,eind[0])
-    ampmn, amprms= grid_var(amplitude,hist,ind)
-    lenmn, lenrms= grid_var(diameter,hist,ind)
-    wvlenmn, wvlenrms= grid_var(wvdiameter,hist,ind)
-    uglmn, uglrms= grid_var(ugdiameter,hist,ind)
-    rvmn, rvrms= grid_var(relvort,hist,ind)
+def bin_space(lon,lat,eind,amplitude,diameter,relvort,binsize=7,method='mean'):
+    hist, ind, blon, blat = grid_space(lon,lat,eind[0],binsize=binsize)
+    ampmn, amprms= grid_var(amplitude,hist,ind,method=method)
+    lenmn, lenrms= grid_var(diameter,hist,ind,method=method)
+#    wvlenmn, wvlenrms= grid_var(wvdiameter,hist,ind,method=method)
+#    uglmn, uglrms= grid_var(ugdiameter,hist,ind,method=method)
+    rvmn, rvrms= grid_var(relvort,hist,ind,method=method)
     
-    return blon, blat, hist,  ampmn, lenmn, rvmn, uglmn, wvlenmn, amprms, lenrms, rvrms, uglrms, wvlenrms
+    return blon, blat, hist,  ampmn, lenmn, rvmn, amprms, lenrms, rvrms
 
-def bin_time(time,eind,amplitude,diameter,relvort,ugdiameter,wvdiameter):
+def bin_time(time,eind,amplitude,diameter,relvort,binsize=1,method='mean'):
     date,datetime=AT.cnes_convert(time)
-    thist,tind,btime=grid_time(time,eind[1])
-    trvmn,trvrms=grid_var(relvort,thist,tind)
-    tampmn,tamprms=grid_var(amplitude,thist,tind)
-    tlenmn,tlenrms=grid_var(diameter,thist,tind)
-    twvlmn,twvlrms=grid_var(wvdiameter,thist,tind)
-    tuglmn,tuglrms=grid_var(ugdiameter,thist,tind)
-    return datetime, btime, thist, tampmn, tlenmn, trvmn, tuglmn, twvlmn, tamprms, tlenrms, trvrms, tuglrms, twvlrms
+    thist,tind,btime=grid_time(time,eind[1],binsize=binsize)
+    trvmn,trvrms=grid_var(relvort,thist,tind,method=method)
+    tampmn,tamprms=grid_var(amplitude,thist,tind,method=method)
+    tlenmn,tlenrms=grid_var(diameter,thist,tind,method=method)
+#    twvlmn,twvlrms=grid_var(wvdiameter,thist,tind,method=method)
+#    tuglmn,tuglrms=grid_var(ugdiameter,thist,tind,method=method)
+    return datetime, btime, thist, tampmn, tlenmn, trvmn, tamprms, tlenrms, trvrms
