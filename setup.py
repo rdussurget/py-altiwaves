965235a6a5ec487c2a444a1d38ff2d4f931516f5
Renaud Dussurget <rdussurget@cls.fr>
2013-10-15 09:45:16 +0200

    preparing relased v2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='py-altiwaves',
-    version='0.1.0',
+    version='0.2.0',
     author='R. Dussurget',
     author_email='renaud.dussurget@gmail.com',
     packages=['kernel','external'],
